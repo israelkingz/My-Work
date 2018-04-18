516e7cfc7e03d057a6d67fb6b6ee1cb5262e512c
israelkingz <iodeajo@gmail.com>
2018-04-18 00:55:28 -0700

    Add files via upload

---
+++ use.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="no-js ie9" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en-US" prefix="og: http://ogp.me/ns#"><!--<![endif]-->
+
+
+<!-- Mirrored from jaipurprinters.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Mar 2018 17:16:31 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+ <link rel="icon" href="images/logo.png"/>  
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>HNG Internship </title>
+<link rel="pingback" href="xmlrpc.php">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K422V34');</script>
+<!-- End Google Tag Manager -->
+<!-- This site is optimized with the Yoast SEO plugin v3.0.7 - https://yoast.com/wordpress/plugins/seo/ -->
+<meta name="description" content="Jaipur Printers is one of the Oldest Printing Company in India providing Production Services for Digital Printing, Offset Printing and Customized Printing."/>
+<meta name="robots" content="noodp,noydir"/>
+<link rel="canonical" href="index.html" />
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Jaipur Printers | Quality Printing Press in India - Since 1948" />
+<meta property="og:description" content="Jaipur Printers is one of the Oldest Printing Company in India providing Production Services for Digital Printing, Offset Printing and Customized Printing." />
+<meta property="og:url" content="https://jaipurprinters.com/" />
+<meta property="og:site_name" content="Jaipur Printers" />
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:description" content="Jaipur Printers is one of the Oldest Printing Company in India providing Production Services for Digital Printing, Offset Printing and Customized Printing."/>
+<meta name="twitter:title" content="Jaipur Printers | Quality Printing Press in India - Since 1948"/>
+<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","url":"https:\/\/jaipurprinters.com\/","name":"Jaipur Printers","alternateName":"Printing Press in India","potentialAction":{"@type":"SearchAction","target":"https:\/\/jaipurprinters.com\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"Organization","url":"https:\/\/jaipurprinters.com\/","sameAs":[],"name":"Jaipur Printers","logo":"https:\/\/jaipurprinters.com\/wp-content\/uploads\/2015\/12\/Jaipur-Printers.png"}</script>
+<meta name="google-site-verification" content="49qbCvqcLcWjOr1DsQz6-nMLsPoYYQUYpwgswyuXrmE" />
+<!-- / Yoast SEO plugin. -->
+
+<link rel='dns-prefetch' href='http://fonts.googleapis.com/' />
+<link rel='dns-prefetch' href='http://s.w.org/' />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.4\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/jaipurprinters.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.9.4"}};
+			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56692,8205,9792,65039],[55357,56692,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='x-stack-css'  href='wp-content/themes/x/framework/css/site/stacks/renew274c.css?ver=4.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='x-google-fonts-css'  href='http://fonts.googleapis.com/css?family=Lato%3A400%2C400italic%2C700%2C700italic&amp;subset=latin%2Clatin-ext&amp;ver=4.3.2' type='text/css' media='all' />
+<script type='text/javascript' src='wp-includes/js/jquery/jqueryb8ff.js?ver=1.12.4'></script>
+<script type='text/javascript' src='wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1'></script>
+<script type='text/javascript' src='wp-content/themes/x/framework/js/dist/site/x-head.min274c.js?ver=4.3.2'></script>
+<script type='text/javascript' src='https://jaipurprinters.com/wp-content/plugins/cornerstone/assets/js/dist/site/cs-head.min.js?ver=1.1.0'></script>
+<link rel='https://api.w.org/' href='wp-json/index.html' />
+<link rel="alternate" type="application/json+oembed" href="wp-json/oembed/1.0/embedc0ca.json?url=https%3A%2F%2Fjaipurprinters.com%2F" />
+<link rel="alternate" type="text/xml+oembed" href="wp-json/oembed/1.0/embed65db?url=https%3A%2F%2Fjaipurprinters.com%2F&amp;format=xml" />
+<style id="x-generated-css" type="text/css">a,h1 a:hover,h2 a:hover,h3 a:hover,h4 a:hover,h5 a:hover,h6 a:hover,.x-comment-time:hover,#reply-title small a,.comment-reply-link:hover,.x-comment-author a:hover,.x-recent-posts a:hover .h-recent-posts{color:#ed1a3b;}a:hover,#reply-title small a:hover{color:#ed1a3b;}.entry-title:before{color:#dddddd;}a.x-img-thumbnail:hover,li.bypostauthor > article.comment{border-color:#ed1a3b;}.flex-direction-nav a,.flex-control-nav a:hover,.flex-control-nav a.flex-active,.x-dropcap,.x-skill-bar .bar,.x-pricing-column.featured h2,.h-comments-title small,.x-pagination a:hover,.x-entry-share .x-share:hover,.entry-thumb,.widget_tag_cloud .tagcloud a:hover,.widget_product_tag_cloud .tagcloud a:hover,.x-highlight,.x-recent-posts .x-recent-posts-img:after,.x-portfolio-filters{background-color:#ed1a3b;}.x-portfolio-filters:hover{background-color:#ed1a3b;}.x-main{width:64.79803%;}.x-sidebar{width:28.79803%;}.x-topbar .p-info,.x-topbar .p-info a,.x-topbar .x-social-global a{color:#ffffff;}.x-topbar .p-info a:hover{color:#959baf;}.x-topbar{background-color:#ffffff;}.x-navbar .desktop .x-nav > li:before{padding-top:37px;}.x-navbar .desktop .x-nav > li > a,.x-navbar .desktop .sub-menu li > a,.x-navbar .mobile .x-nav li a{color:#999999;}.x-navbar .desktop .x-nav > li > a:hover,.x-navbar .desktop .x-nav > .x-active > a,.x-navbar .desktop .x-nav > .current-menu-item > a,.x-navbar .desktop .sub-menu li > a:hover,.x-navbar .desktop .sub-menu li.x-active > a,.x-navbar .desktop .sub-menu li.current-menu-item > a,.x-navbar .desktop .x-nav .x-megamenu > .sub-menu > li > a,.x-navbar .mobile .x-nav li > a:hover,.x-navbar .mobile .x-nav li.x-active > a,.x-navbar .mobile .x-nav li.current-menu-item > a{color:#ed1a3b;}.x-btn-navbar,.x-btn-navbar:hover{color:#ffffff;}.x-navbar .desktop .sub-menu li:before,.x-navbar .desktop .sub-menu li:after{background-color:#999999;}.x-navbar,.x-navbar .sub-menu{background-color:#ffffff !important;}.x-btn-navbar,.x-btn-navbar.collapsed:hover{background-color:#ed1a3b;}.x-btn-navbar.collapsed{background-color:#ed1a3b;}.x-navbar .desktop .x-nav > li > a:hover > span,.x-navbar .desktop .x-nav > li.x-active > a > span,.x-navbar .desktop .x-nav > li.current-menu-item > a > span{box-shadow:0 2px 0 0 #ed1a3b;}.x-navbar .desktop .x-nav > li > a{height:90px;padding-top:37px;}.x-navbar .desktop .x-nav > li ul{top:90px;}.x-colophon.bottom{background-color:#000000;}.x-colophon.bottom,.x-colophon.bottom a,.x-colophon.bottom .x-social-global a{color:#ffffff;}.h-landmark{font-weight:400;}.x-comment-author a{color:#444444;}.x-comment-author a,.comment-form-author label,.comment-form-email label,.comment-form-url label,.comment-form-rating label,.comment-form-comment label,.widget_calendar #wp-calendar caption,.widget_calendar #wp-calendar th,.x-accordion-heading .x-accordion-toggle,.x-nav-tabs > li > a:hover,.x-nav-tabs > .active > a,.x-nav-tabs > .active > a:hover{color:#000000;}.widget_calendar #wp-calendar th{border-bottom-color:#000000;}.x-pagination span.current,.x-portfolio-filters-menu,.widget_tag_cloud .tagcloud a,.h-feature-headline span i,.widget_price_filter .ui-slider .ui-slider-handle{background-color:#000000;}@media (max-width:979px){}body{font-size:18px;font-style:normal;font-weight:400;color:#444444;background-color:#ffffff;}a:focus,select:focus,input[type="file"]:focus,input[type="radio"]:focus,input[type="submit"]:focus,input[type="checkbox"]:focus{outline:thin dotted #333;outline:5px auto #ed1a3b;outline-offset:-1px;}h1,h2,h3,h4,h5,h6,.h1,.h2,.h3,.h4,.h5,.h6{font-family:"Lato",sans-serif;font-style:normal;font-weight:700;}h1,.h1{letter-spacing:0.005em;}h2,.h2{letter-spacing:0.005em;}h3,.h3{letter-spacing:0.005em;}h4,.h4{letter-spacing:0.005em;}h5,.h5{letter-spacing:0.005em;}h6,.h6{letter-spacing:0.005em;}.w-h{font-weight:700 !important;}.x-container.width{width:75%;}.x-container.max{max-width:1200px;}.x-main.full{float:none;display:block;width:auto;}@media (max-width:979px){.x-main.full,.x-main.left,.x-main.right,.x-sidebar.left,.x-sidebar.right{float:none;display:block;width:auto !important;}}.entry-header,.entry-content{font-size:18px;}body,input,button,select,textarea{font-family:"Lato",sans-serif;}h1,h2,h3,h4,h5,h6,.h1,.h2,.h3,.h4,.h5,.h6,h1 a,h2 a,h3 a,h4 a,h5 a,h6 a,.h1 a,.h2 a,.h3 a,.h4 a,.h5 a,.h6 a,blockquote{color:#000000;}.cfc-h-tx{color:#000000 !important;}.cfc-h-bd{border-color:#000000 !important;}.cfc-h-bg{background-color:#000000 !important;}.cfc-b-tx{color:#444444 !important;}.cfc-b-bd{border-color:#444444 !important;}.cfc-b-bg{background-color:#444444 !important;}body.x-navbar-fixed-top-active .x-navbar-wrap{height:90px;}.x-navbar-inner{min-height:90px;}.x-brand{margin-top:12px;font-family:"Lato",sans-serif;font-size:42px;font-style:normal;font-weight:700;letter-spacing:-0.035em;color:#000000;}.x-brand:hover,.x-brand:focus{color:#000000;}.x-brand img{width:187.5px;}.x-navbar .x-nav-wrap .x-nav > li > a{font-family:"Lato",sans-serif;font-style:normal;font-weight:700;letter-spacing:0.085em;text-transform:uppercase;}.x-navbar .desktop .x-nav > li > a{font-size:13px;}.x-navbar .desktop .x-nav > li > a:not(.x-btn-navbar-woocommerce){padding-left:20px;padding-right:20px;}.x-navbar .desktop .x-nav > li > a > span{margin-right:-0.085em;}.x-btn-navbar{margin-top:28px;}.x-btn-navbar,.x-btn-navbar.collapsed{font-size:18px;}@media (max-width:979px){body.x-navbar-fixed-top-active .x-navbar-wrap{height:auto;}.x-widgetbar{left:0;right:0;}}.x-btn,.button,[type="submit"]{color:#ffffff;border-color:#ed1a3b;background-color:#ed1a3b;text-shadow:0 0.075em 0.075em rgba(0,0,0,0.5);border-radius:0.25em;}.x-btn:hover,.button:hover,[type="submit"]:hover{color:#ffffff;border-color:#ed1a3b;background-color:#ed1a3b;text-shadow:0 0.075em 0.075em rgba(0,0,0,0.5);}.x-btn.x-btn-real,.x-btn.x-btn-real:hover{margin-bottom:0.25em;text-shadow:0 0.075em 0.075em rgba(0,0,0,0.65);}.x-btn.x-btn-real{box-shadow:0 0.25em 0 0 #ed1a3b,0 4px 9px rgba(0,0,0,0.75);}.x-btn.x-btn-real:hover{box-shadow:0 0.25em 0 0 #ed1a3b,0 4px 9px rgba(0,0,0,0.75);}.x-btn.x-btn-flat,.x-btn.x-btn-flat:hover{margin-bottom:0;text-shadow:0 0.075em 0.075em rgba(0,0,0,0.65);box-shadow:none;}.x-btn.x-btn-transparent,.x-btn.x-btn-transparent:hover{margin-bottom:0;border-width:3px;text-shadow:none;text-transform:uppercase;background-color:transparent;box-shadow:none;}body{-webkit-font-smoothing:antialiased!important;-moz-font-smoothing:antialiased!important;-moz-osx-font-smoothing: grayscale;}
+
+.x-btn{
+font-family: 'Lato', sans-serif;
+text-shadow:none;
+font-weight:700;
+}
+
+.x-counter{font-family: 'Lato', sans-serif;}</style><style id="cornerstone-custom-page-css" type="text/css"></style></head>
+
+<body class="home page-template page-template-template-blank-4 page-template-template-blank-4-php page page-id-5 x-renew x-navbar-fixed-top-active x-full-width-layout-active x-content-sidebar-active x-post-meta-disabled x-v4_3_2 cornerstone-v1_1_0">
+
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K422V34"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+  
+  <div id="top" class="site">
+
+  
+  
+  <header class="masthead masthead-inline" role="banner">
+    
+  
+
+  <div class="x-navbar-wrap">
+    <div class="x-navbar">
+      <div class="x-navbar-inner">
+        <div class="x-container max width">
+          
+<h1 class="visually-hidden">HNG Internship</h1>
+       
+<a href="#" class="x-btn-navbar collapsed" data-toggle="collapse" data-target=".x-nav-wrap.mobile">
+  <i class="x-icon-bars" data-x-icon="&#xf0c9;"></i>
+  <span class="visually-hidden">Navigation</span>
+</a>
+
+
+      </div>
+    </div>
+  </div>
+
+  </header>
+
+    
+
+  <div class="x-main full" role="main">
+
+    
+      <article id="post-5" class="post-5 page type-page status-publish hentry no-post-thumbnail">
+        
+
+<div class="entry-content content">
+
+
+  <div id="x-section-1" class="x-section left-text " style=" margin: 0px; padding: 85px 0px 175px; background-color: #f9f9f9;" ><div  class="x-container max width" style="margin: 0px auto; padding: 0px; "><div  class="x-column x-sm center-text  x-1-1" style="padding: 0px; "  ><h1  class="h-custom-headline   h2 accent" ><span>WELCOME TO HNG INTERNSHIP 4.0</span></h1><div  class="x-text"    ><p>   
+</div><a  class="x-btn x-btn-large"  href="printing-services.html"     data-options="thumbnail: ''"> 
+
+Today;s Date Is: <br>
+<?php echo(strftime("%m/%d/%Y")); ?></p>
+
+<p>The Time is : <br>
+<?php echo (strftime("%H:%M")); ?></p></a></div></div></div><div id="x-section-2" class="x-section" style=" margin: 0px; padding: 25px 0px 55px; background-color: transparent;" ><div  class="x-container max width" style="margin: 0px auto; padding: 0px; "><div  class="x-column x-sm x-1-1" style="padding: 0px; "  ><h2  class="h-custom-headline h2" >
+
+  
+
+  
+  </div> <!-- END #top.site -->
+
+  
+
+  
+</body>
+
+<!-- Mirrored from jaipurprinters.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 26 Mar 2018 17:17:00 GMT -->
+</html>
